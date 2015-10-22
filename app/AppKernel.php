d293bf69b11bad5c9b52ef6f0bd5bd13a1d9546e
sergiombravo <sergio.mbravo@gmail.com>
2015-10-22 11:33:15 +0200

    añadidos los estilos de login. Añadida ruta app/Kernel.php al gitignore

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -5,6 +5,11 @@ use Symfony\Component\Config\Loader\LoaderInterface;
 
 class AppKernel extends Kernel
 {
+	public function init(){
+		date_default_timezone_set('Europe/Paris');
+		parent::init();
+	}
+
     public function registerBundles()
     {
         $bundles = [
